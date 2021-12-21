5eb4a3b7eb16bda81bed56b1541308d80002c902
Yoni <yoni@fixel.ai>
2021-12-21 14:23:42 +0200

    feat: added 2 line support for post card with ellipsis

--- src/lib/posts/PostCard.svelte
+++ src/lib/posts/PostCard.svelte
@@ -2,10 +2,12 @@
     export let post: Post
 </script>
 
-<a class="card block shadow-md rounded p-4 w-full orange-glow" sveltekit:prefetch href="{`/posts/${post.id}`}">
+<a class="card block shadow-md rounded p-4 w-full orange-glow max-h-72" sveltekit:prefetch href="{`/posts/${post.id}`}">
     <div>
         <img src="{post.jetpack_featured_media_url}" alt="Post">
-        <div>{@html post.title.rendered}</div>
+        <div class="box text">{@html post.title.rendered}
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam vitae consequuntur nemo repudiandae qui atque, porro illum accusamus modi reprehenderit eaque commodi aliquid suscipit consequatur laborum incidunt excepturi nisi.</span>
+        </div>
     </div>
 </a>
 
