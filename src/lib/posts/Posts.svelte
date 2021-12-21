19ac0da37c71945d45915bbc3bf09177c476a7d3
Yoni <yoni@fixel.ai>
2021-12-21 13:51:05 +0200

    feat: changes to Posts

--- src/lib/posts/Posts.svelte
+++ src/lib/posts/Posts.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { getPosts } from "../../services/wordpress";
 import { onMount } from "svelte";
+import PostCard from "./PostCard.svelte";
 
 let posts: Array<Post>;
     onMount( async ()=> {
@@ -13,15 +14,10 @@ let posts: Array<Post>;
     })
 </script>
 
-<div class="grid mt-12 md:grid-cols-3 sm:grid-cols-1 justify-items-center">        
+<div class="grid mt-12 md:grid-cols-3 sm:grid-cols-1 justify-items-center gap-4">        
     {#if posts != null}
         {#each posts as post}
-            <a sveltekit:prefetch href="{`/posts/${post.id}`}">
-                <div class="block">
-                    <img src="{post.jetpack_featured_media_url}" alt="Post">
-                    <div>{@html post.title.rendered}</div>
-                </div>
-            </a>
+            <PostCard post={post}/>
         {/each}
     {/if}
 </div>
