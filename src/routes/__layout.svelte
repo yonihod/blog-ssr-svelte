b06bc2fb4a63d16d115b2930827f463f2156ab67
yonih <yonihod199@gmail.com>
2021-12-31 18:35:14 +0200

    feat: added gtag analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,13 @@
 </script>
 
 <div id="root" class="lg:w-1/2 md:w-full p-4 mx-auto relative">
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-EYG3T2R6M8"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){window.dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-EYG3T2R6M8');
+	</script>
 	<Header/>
 	<slot/>
 	<Footer/>
