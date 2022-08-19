f90d46974eaefd9f0cb79a5c154d358df9f3506e
yonih <yonihod199@gmail.com>
2022-08-19 15:17:56 +0300

    disabled prism for now til it fixed

--- src/lib/actions/codify.ts
+++ src/lib/actions/codify.ts
@@ -3,6 +3,6 @@ import Prism from 'prismjs';
 export default function codify(node: HTMLElement) {
     const codeTag = Array.from(node.getElementsByTagName("code"));
     codeTag.forEach(code => {
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages[code.dataset.lang] || Prism.languages.javascript);
+        // code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages['jsx'] || Prism.languages.javascript);
     });
 }
